b90d9865627f8b1fee1579732485750f6c6238dc
suzhanlee <108535068+suzhanlee@users.noreply.github.com>
2024-07-10 00:35:53 +0900

    다시 action

--- src/main/java/aws/vpc/subnet/SubnetConfig.java
+++ src/main/java/aws/vpc/subnet/SubnetConfig.java
@@ -22,16 +22,16 @@ public class SubnetConfig {
         List<SubnetDto> subnetDtos = new ArrayList<>();
 
         PublicSubnet publicSubnet1 = new PublicSubnet(scope, vpc);
-        subnetDtos.add(publicSubnet1.configSubnet("PublicSubnet1", "20.0.1.0/24", AZ_1A));
+        subnetDtos.add(publicSubnet1.configSubnet("PublicSubnet1", "20.0.10.0/24", AZ_1A));
 
         PublicSubnet publicSubnet2 = new PublicSubnet(scope, vpc);
-        subnetDtos.add(publicSubnet2.configSubnet("PublicSubnet2", "20.0.2.0/24", AZ_1B));
+        subnetDtos.add(publicSubnet2.configSubnet("PublicSubnet2", "20.0.20.0/24", AZ_1B));
 
         PrivateSubnet privateSubnet1 = new PrivateSubnet(scope, vpc);
-        subnetDtos.add(privateSubnet1.configSubnet("PrivateSubnet1", "20.0.3.0/24", AZ_1A));
+        subnetDtos.add(privateSubnet1.configSubnet("PrivateSubnet1", "20.0.30.0/24", AZ_1A));
 
         PrivateSubnet privateSubnet2 = new PrivateSubnet(scope, vpc);
-        subnetDtos.add(privateSubnet2.configSubnet("PrivateSubnet2", "20.0.4.0/24", AZ_1B));
+        subnetDtos.add(privateSubnet2.configSubnet("PrivateSubnet2", "20.0.40.0/24", AZ_1B));
 
         return subnetDtos;
     }
