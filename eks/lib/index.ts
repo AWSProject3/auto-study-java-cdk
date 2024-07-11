64ced146dea3e334a84dd97b1d86a687cd73419a
suzhanlee <108535068+suzhanlee@users.noreply.github.com>
2024-07-11 23:17:53 +0900

    .gitignore 수정으로 올라가지 못한 eks 폴더 푸시

---
+++ eks/lib/index.ts
@@ -0,0 +1,85 @@
+import * as cdk from 'aws-cdk-lib';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import * as iam from 'aws-cdk-lib/aws-iam';
+import * as blueprints from '@aws-quickstart/eks-blueprints';
+import {Construct} from 'constructs';
+
+export class EksConfigStack extends cdk.Stack {
+    constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+        super(scope, id, props);
+
+        const vpcId = cdk.Fn.importValue('auto-study-vpc-id');
+        const publicSubnetId1 = cdk.Fn.importValue('publicSubnetId us-east-2a');
+        const publicSubnetId2 = cdk.Fn.importValue('publicSubnetId us-east-2b');
+        const privateSubnetId1 = cdk.Fn.importValue('privateSubnetId us-east-2a');
+        const privateSubnetId2 = cdk.Fn.importValue('privateSubnetId us-east-2b');
+
+        const vpc = ec2.Vpc.fromVpcAttributes(this, 'ExistingVpc', {
+            vpcId: vpcId,
+            availabilityZones: ['us-east-2a', 'us-east-2b'],
+            publicSubnetIds: [publicSubnetId1, publicSubnetId2],
+            privateSubnetIds: [privateSubnetId1, privateSubnetId2],
+        });
+
+        const privateSubnets = vpc.selectSubnets({subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS});
+
+        const clusterProvider = new blueprints.GenericClusterProvider({
+            version: cdk.aws_eks.KubernetesVersion.V1_27,
+            mastersRole: this.createMasterRole(),
+            managedNodeGroups: [
+                {
+                    id: "OnDemandNodes",
+                    instanceTypes: [new ec2.InstanceType('m5.xlarge')],
+                    minSize: 3,
+                    maxSize: 6,
+                    desiredSize: 3,
+                    nodeGroupCapacityType: cdk.aws_eks.CapacityType.ON_DEMAND,
+                    nodeGroupSubnets: privateSubnets,
+                },
+            ],
+            vpcSubnets: [privateSubnets],
+        });
+
+        const blueprint = blueprints.EksBlueprint.builder()
+            .account(this.account)
+            .region(this.region)
+            .addOns(
+                new blueprints.addons.VpcCniAddOn(),
+                new blueprints.addons.CoreDnsAddOn(),
+                new blueprints.addons.KubeProxyAddOn(),
+                new blueprints.addons.AwsLoadBalancerControllerAddOn(),
+                new blueprints.addons.ArgoCDAddOn()
+            )
+            .clusterProvider(clusterProvider)
+            .resourceProvider(blueprints.GlobalResources.Vpc, new blueprints.VpcProvider(vpc.vpcId))
+            .build(this, 'auto-study-eks');
+
+        this.tagSubnets(vpc, 'auto-study-eks');
+    }
+
+    private createMasterRole(): iam.Role {
+        return new iam.Role(this, 'MasterRole', {
+            assumedBy: new iam.AccountRootPrincipal(),
+        });
+    }
+
+    private tagSubnets(vpc: ec2.IVpc, clusterName: string) {
+        vpc.publicSubnets.forEach(subnet => {
+            cdk.Tags.of(subnet).add('kubernetes.io/role/elb', '1');
+        });
+
+        vpc.privateSubnets.forEach(subnet => {
+            cdk.Tags.of(subnet).add('kubernetes.io/role/internal-elb', '1');
+            cdk.Tags.of(subnet).add(`kubernetes.io/cluster/${clusterName}`, 'shared');
+        });
+    }
+}
+
+const app = new cdk.App();
+new EksConfigStack(app, 'EksConfigStack', {
+    env: {
+        account: process.env.CDK_DEFAULT_ACCOUNT,
+        region: process.env.CDK_DEFAULT_REGION
+    }
+});
+app.synth();
